26c59923d253cc5ee0f7efbc0d443329e2f9e1d1
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-05-18 10:14:32 -0500

    complete promise exercise

--- js/promises.js
+++ js/promises.js
@@ -1,26 +1,66 @@
-const username = fetch('https://api.github.com/users', {headers: {'Authorization': YOUR_TOKEN_HERE}});
-console.log(username);
-
-// function remove9s(arr) {
-//     var no9s = [];
-//     arr.forEach(function (element) {
-//         if (element !== 9) {
-//             no9s.push(element);
-//         }
-//     });
-//     return no9s;
-// }
-
-function remove9s(arr) {
-    var no9s = [];
-    arr.forEach(function (a) {
-        if (a !== 3 && a !== 6) {
-            no9s.push(a)
+const userLastCommit = username => fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${GITHUB_ACCESS_TOKEN}`}})
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data);
+        let lastPush;
+        for (let event of data) {
+            if (event.type === 'PushEvent') {
+                lastPush = new Date(event.created_at);
+                break;
+            }
         }
+        console.log(lastPush);
     });
-    return no9s;
+
+
+userLastCommit('carlosmunozus03');
+
+//wait
+
+const wait = ms => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
 }
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+wait(5000).then(() => console.log('You\'ll see this after 5 seconds'));
+// console.log(username);
+//
+// //here no work
+// const getData = () => {
+//     return username
+// }
+// console.log(getData());
+//
+// // here work
+// const getDataTime = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(username);
+//         }, 1000);
+//     });
+// }
+// // console.log(getDataTime());
+// getDataTime().then((username) => console.log(username));
+//
+//
+// ///// other exercise
+//
+// const sayHello = new Promise((resolve, reject) => {
+//     resolve('hello');
+//     reject('no see this');
+// });
+// console.log(sayHello)
+// sayHello.then(function (response) {
+//     console.log('the promise is resolve ', response);
+// }).catch(function (error) {
+//     console.log('the promise it no work and it a ', error);
+// }).finally(function () {
+//     console.log('the end');
+// });
 
+//here is the work
 
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(remove9s(arr))
