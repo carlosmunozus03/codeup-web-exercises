52968f3c2bbb72e832a22b41ebcd02208065d863
Carlos Munoz <carlos.munoz.us03@gmail.com>
2022-02-15 11:04:50 -0600

    test js

--- js/test.js
+++ js/test.js
@@ -1,11 +1,28 @@
 "use strict"
 
-var shapes = ['square', 'circle','triangle', 'rectangle'];
+var shapes = ["square", "circle", "triangle", "rectangle"];
 
 for (var i = 0; i < shapes.length; i++) {
-console.log("The following shapes is "+shapes[i]+ " in the index "+i);
-};
+    console.log("The following shapes is " + shapes[i] + " in the index " + i);
+}
+;
 
 console.log("----------------------");
 
-shapes.forEach(function(shape,index) { console.log("shapes is "+shape+" and the index "+index); });
+shapes.forEach(function (shape, index) {
+    console.log("shapes is " + shape + " and the index " + index);
+});
+
+shapes.unshift("hexagon");
+console.log(shapes);
+shapes.push("oval", "diamond");
+console.log(shapes);
+
+shapes.forEach(function (shape, index) {
+    console.log("The next shape is " + shape + " and the index " + index);
+});
+shapes.shift();
+console.log(shapes);
+shapes.pop();
+console.log(shapes);
+
